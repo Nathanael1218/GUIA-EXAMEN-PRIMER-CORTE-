db783ac52f040c2385912e266749c4a01df641b6
Nathanael1218 <55523362+Nathanael1218@users.noreply.github.com>
2019-10-15 13:41:09 -0500

    Add files via upload

---
+++ ACTIVIDAD 9.c
@@ -0,0 +1,19 @@
+/*HERNANDEZ GAONA VICTOR NATHANAEL
+1AV6   ACTIVIDAD 9*/
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+int main(void){
+int z,x=2;
+if(3!=x){
+if(x==1){
+z=1;
+ }
+else
+ z=x;
+ }
+else
+ z=0;
+printf("El resultado de Z es:%d",z);
+}
