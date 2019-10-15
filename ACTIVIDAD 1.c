9499804383d9385c99b2ec41a77dba6e1da36af9
Nathanael1218 <55523362+Nathanael1218@users.noreply.github.com>
2019-10-15 01:22:11 -0500

    Add files via upload

---
+++ ACTIVIDAD 1.c
@@ -0,0 +1,13 @@
+/*HERNANDEZ GAONA VICTOR NATHANAEL
+1AV6 ACTIVIDAD 1*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main (void){
+int a=4, b=10, c=4;
+float z, d=1.1;
+z=((a+b)/c)*d;
+printf("%f",z);
+}
