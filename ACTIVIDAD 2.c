ff070d164cd9bc55837099f5db5f7d42afb1a810
Nathanael1218 <55523362+Nathanael1218@users.noreply.github.com>
2019-10-15 01:30:44 -0500

    Add files via upload

---
+++ ACTIVIDAD 2.c
@@ -0,0 +1,36 @@
+/*HERNANDEZ GAONA VICTOR NATHANAEL
+   1AV6   ACTIVIDAD2 */
+
+#include<stdio.h>
+#include<conio.h>
+int main (void){
+int n=2,m=5,a,b,c,d;
+a=(n+m)==2;
+b=(n>m)==!(m==5);
+c=(m%n)>5;
+d=((m-n)*10)%4==1;
+if(a==0){
+ printf("La operacion (n+m)==2 es falsa.\n");
+ }
+else{
+printf("La operacion (n+m)==2 es verdadera.\n");
+}
+if(b==0){
+ printf("La operacion (n>m)==!(m==5) es falsa.\n");
+ }
+else{
+printf("La operacion (n>m)==!(m==5) es verdadera.\n");
+}
+if(c==0){
+ printf("La operacion (el modulo de m con respecto a n)>5 es falsa.\n");
+ }
+else{
+printf("La operacion (el modulo de m con respecto a n)>5 es verdadera.\n");
+}
+if(d==0){
+ printf("La operacion (el modulo de (m-n)*10) con respecto a 4==1 es falsa.\n");
+ }
+else{
+printf("La operacion (el modulo de (m-n)*10) con respecto a 4==1 es verdadera.\n");
+}
+}
