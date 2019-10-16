202a31ff084af10de44a7af30a8756446fac8f79
Nathanael1218 <55523362+Nathanael1218@users.noreply.github.com>
2019-10-15 22:23:27 -0500

    Add files via upload

---
+++ ACTIVIDAD 16.c
@@ -0,0 +1,14 @@
+/*HERNANDEZ GAONA VICTOR NATHANAEL
+1AV6   ACTIVIDAD 16*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+int main (void){
+int a=9,i;
+for (i=0;i<100;i++){
+ if((a==a%4)||(i%2)==0){
+  printf("%d\n",a);
+  }
+ }
+}
